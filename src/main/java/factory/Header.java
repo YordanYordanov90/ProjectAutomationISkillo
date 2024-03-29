25a8db2a742265a053525a675b64d92e571eede7
YordanYordanov90 <157980191+YordanYordanov90@users.noreply.github.com>
2024-03-29 21:04:59 +0200

    new

---
+++ src/main/java/factory/Header.java
@@ -0,0 +1,41 @@
+package factory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Header {
+    private final WebDriver webDriver;
+    @FindBy(id = "nav-link-login")
+    private WebElement logInLink;
+    @FindBy(id = "nav-link-profile")
+    private WebElement profilePageLink;
+    @FindBy(id = "nav-link-new-post")
+    private WebElement newPostLink;
+
+
+    public Header(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    public void clickLogInButton(){
+        logInLink.click();
+    }
+
+    public void clickProfileButton(){
+        WebDriverWait wait = new WebDriverWait(this.webDriver, Duration.ofSeconds(15));
+        wait.until(ExpectedConditions.elementToBeClickable(profilePageLink));
+        profilePageLink.click();
+    }
+
+    public void clickNewPostButton(){
+        newPostLink.click();
+    }
+    boolean isLogInLinkEnabled(){
+        return logInLink.isEnabled();
+    }
+}
